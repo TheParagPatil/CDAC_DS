a764bdade7ae46cfb013f5304dadcb4d47832cd0
Parag Patil <37205676+TheParagPatil@users.noreply.github.com>
2022-11-17 16:23:53 +0530

    Update LinearProbingTest.java

--- HashSet/LinearProbingTest.java
+++ HashSet/LinearProbingTest.java
@@ -7,9 +7,31 @@ public class LinearProbingTest {
 		linProbHs.insertElement("uk", "London");
 		linProbHs.insertElement("chi", "China");
 		linProbHs.insertElement("uae", "Emirates");
+
+		System.out.println("-------------");
 		
-		String countryName=linProbHs.get("ukk");
-		System.out.println("CountryName : "+countryName);
+		linProbHs.printHashTableValues();
+		
+		linProbHs.remove("chi");
+		
+		linProbHs.printHashTableValues();
+		
+		/*String countryName=linProbHs.get("ukk");
+		System.out.println("CountryName : "+countryName);*/
+		
+		/*String keyToSearch = "uk";
+		boolean status = linProbHs.containsKey(keyToSearch);
+		System.out.println("status "+status);
+		
+		if(status ==true) {
+			System.out.println("Key found");
+			String value = linProbHs.get(keyToSearch);
+			System.out.println("Key's value : "+value);
+		}
+		else {
+			System.out.println("Key not found...");
+		}*/
+		
 		
 		
 	}
@@ -28,19 +50,44 @@ class LinearProbingHashSet
 		vals = new String [ maxSize];
 	}
 	
+	public void printHashTableValues() {
+		for (int i = 0; i < maxSize; i++) {
+			
+			if(keys[i]!=null) {
+				System.out.println("Key : "+keys[i]+" and Value : "+vals[i]);
+			}
+		//	System.out.println("\n");
+		}
+	}
+	
 	private int getSize() {
 		return currentSize;
+	}
+	
+	public boolean containsKey(String key) {
+		boolean keyFound=false;		
+		for (int i = 0; i < maxSize; i++) {
+			if(keys[i].equals(key)) {
+				keyFound=true;
+				break;
+			}
+		}
+		return keyFound;
+				
 	}
 	
 	public String get(String keyToFind)
 	{
 		int i = hash(keyToFind);
-		
+ 
 		while(keys[i]!=null) {
-			if(keys[i].equals(keyToFind)) 
-				return vals[i]; //if found return it;
+			
+			if(keys[i].equals(keyToFind)) {
+				return vals[i];
+			}
 			
 			i=(i+1)%maxSize;
+			System.out.println("i:"+i);
 		}
 		return null;
 	}
@@ -71,11 +118,25 @@ class LinearProbingHashSet
 				i = (i+1)%maxSize; // 2%5=2
 		} while (i!=temp);
 	}
+	
+	public void remove(String keyToRemove) {
+		
+		if(containsKey(keyToRemove)) {
+			System.out.println("Key found to remove...");
+			int i = hash(keyToRemove);//3
+			while(!keyToRemove.equals(keys[i])) {
+				i=(i+1)%maxSize;
+			}
+			keys[i] = vals[i] = null;
+			currentSize--;
+		}
+		else {
+			System.out.println("Key not found.....");
+		}
+	}
 }
 
 /*
 
-	private boolean containsKey(String key) {
-		return get(key)!=null;
-	}
+	
 */
