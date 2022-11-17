79c60ec6aae1f29c5584daf820140f2d902510d1
Parag Patil <37205676+TheParagPatil@users.noreply.github.com>
2022-11-17 10:42:31 +0530

    Update QuickSortTest.java

--- SearchingSorting/QuickSortTest.java
+++ SearchingSorting/QuickSortTest.java
@@ -13,41 +13,59 @@ public class QuickSortTest {
 }
 class QuickSort
 {
-												//0			5
-												//0         3
-												//0         2
-												//0         -1
+
 	private int doPartition(int array[], int startPos, int endPos)
 	{
+		System.out.println(">---doPartition----started---------start:"+startPos+"end:"+endPos);
 		int pivot = array [ endPos ];//25 | 19 | 2
+		System.out.println("--pivot--"+pivot);
 		int i = (startPos - 1 );//0-1 = -1
-		
+		System.out.println("--i:"+i);
 				//j=1       j<=2       j++
 		for(int j=startPos; j <=endPos; j++)
 		{	    //   
+			System.out.println("\t--j--"+j);
 			if(array [ j ] < pivot ) {
+				System.out.println("\t\n--if(array[j]<pivot:"+array[j]+"<"+pivot);
 				i++; // i = 2
+				System.out.println("\t--i++:"+i);
+				System.out.println("\t--swapping..."+array[i]+ " and "+array[j]);
 				int tempValue = array [ i] ;
 				array [ i ] = array [j ];
 				array [ j ] = tempValue;
+				System.out.println("\t--after swap : "+Arrays.toString(array));
+			}
+			else {
+				
+				System.out.println("\t\n--NO SWAP--"+array[j]+"<"+pivot +" failed : i:"+i);
+				System.out.println("\t--NO SWAP--"+Arrays.toString(array));
 			}
 		}
+		System.out.println("\n--after for loop : ");
 		int tempValue = array [ i + 1 ]; // 3+1 = 4 
 		array [ i + 1 ] = array [ endPos ];
 		array [ endPos ] = tempValue;
+		System.out.println("\t--swapping...(i+1)+"+(i+1)+":"+array[i+1]+ " and "+array[endPos]+":endPos:"+endPos);
 		System.out.println((i+1)+"\t"+Arrays.toString(array));
+		System.out.println("<---doPartition----complete---------returning -- "+(i+1)+"\n\n");
 		return i+1; //new position  = 4(1st call) 
 	}
+	
 	public void performQuickSort(int array[], int startPos, int endPos)
 	{	//    0      <  5
+		//System.out.println("performQuickSort : "+Arrays.toString(array)+"start:"+startPos+" : endPos:"+endPos);
 		if(startPos < endPos ) {
-					//			         0         5
-					//					 0         3
-					//					 0         2
+			System.out.println("if startPos < endPos "+startPos+"<"+endPos);
 			int p = doPartition(array, startPos, endPos);
 			
+			System.out.println("RI:performQuickSort : "+Arrays.toString(array)+"start:"+startPos+" : p-1:"+(p-1));
 			performQuickSort(array, startPos, p-1);//4 = 3-1=3 ,  2
+			
+			System.out.println("RII:performQuickSort : "+Arrays.toString(array)+"start is (p+1):"+(p+1)+" endPos:"+endPos);
 			performQuickSort(array, p+1, endPos);// 4 = 4+1 = 5
+		}
+		else {
+			System.out.println("\nelse if startPos < endPos FAILED "+startPos+"<"+endPos+"\n");
 		}
 	}
 	public void printArrayContent(int array[], int len) {
