68baffe7210e30b83333a2c15552c11a2dba172c
Parag Patil <37205676+TheParagPatil@users.noreply.github.com>
2022-11-18 11:44:27 +0530

    Add files via upload

--- HashSet/StringTest.java
+++ HashSet/StringTest.java
@@ -7,24 +7,36 @@ public class StringTest {
 		String str3=new String("uk");
 		String str4=new String("chi");
 		String str5=new String("uae");
-		
+		String str6=new String("eur");
+		String str7=new String("nep");
 		
 		System.out.println("str1:"+str1);
 		System.out.println("str2:"+str2);
 		System.out.println("str3:"+str3);
 		System.out.println("str4:"+str4);
 		System.out.println("str5:"+str5);
+		System.out.println("str6:"+str6);
+		System.out.println("str7:"+str7);
 		
 		System.out.println("str1:"+str1.hashCode());
 		System.out.println("str2:"+str2.hashCode());
 		System.out.println("str3:"+str3.hashCode());
 		System.out.println("str4:"+str4.hashCode());
 		System.out.println("str5:"+str5.hashCode());
+		System.out.println("str6:"+str6.hashCode());
+		System.out.println("str7:"+str7.hashCode());
 		
-		System.out.println("str1:"+str1.hashCode()%5);
-		System.out.println("str2:"+str2.hashCode()%5);
-		System.out.println("str3:"+str3.hashCode()%5);
-		System.out.println("str4:"+str4.hashCode()%5);
-		System.out.println("str5:"+str5.hashCode()%5);
+		System.out.println("str1:"+str1.hashCode()%7);
+		System.out.println("str2:"+str2.hashCode()%7);
+		System.out.println("str3:"+str3.hashCode()%7);
+		System.out.println("str4:"+str4.hashCode()%7);
+		System.out.println("str5:"+str5.hashCode()%7);
+		System.out.println("str6:"+str6.hashCode()%7);
+		System.out.println("str7:"+str7.hashCode()%7);
+		
+		int idx = 5 - 3 % 5;
+		System.out.println("idx "+idx);
+		
+		
 	}
 }
